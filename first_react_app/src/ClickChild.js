53320970a8c4e8a59adebf1dacba2579d873e47a
huangqiang1995 <591344256@qq.com>
2019-08-21 16:39:44 +0800

    完成first_react_app的学习

--- first_react_app/src/ClickChild.js
+++ first_react_app/src/ClickChild.js
@@ -7,17 +7,43 @@ class ClickChild extends Component {
     super(props);
     this.onClickIncBtn = this.onClickIncBtn.bind(this);
     this.onClickDecBtn = this.onClickDecBtn.bind(this);
+    this.updateCount = this.updateCount.bind(this);
     this.state = {
       count : props.initValue || 0
     } 
   }
   onClickIncBtn(){
-    this.setState({count: this.state.count + 1});
+    this.updateCount(true);
   }
   onClickDecBtn(){
-    this.setState({count: this.state.count - 1});
+    this.updateCount(false);    
   }
+  
+  updateCount(isInc){
+    const previousValue = this.state.count;
+    const newValue = isInc?previousValue+1:previousValue-1;
+    this.setState({count:newValue});
+    this.props.onUpdate(newValue,previousValue);
+  }
+
+  componentWillMount(){
+    console.log("enter componentWillMount "+this.props.caption)
+  }
+
+  componentDidMount(){
+    console.log("enter componentDidMount "+this.props.caption)
+  }
+
+  componentWillReceiveProps(nextProps){//只要父组件重新渲染就会引发次钩子，不论props的值是否有变化
+    console.log("enter componentWillReceiveProps "+this.props.caption)
+  }
+
+  shouldComponentUpdate(nextProps,nextState){
+    return (this.props.caption !== nextProps.caption)  || (this.state.count !== nextState.count)
+  }
+
   render(){
+    console.log("enter render " + this.props.caption)
     const {caption} = this.props;
     return(
       <div>
@@ -28,11 +54,16 @@ class ClickChild extends Component {
     );
   }
 }
- 
-console.log(PropTypes)
+
 ClickChild.propTypes = {
   caption: PropTypes.string.isRequired,
-  initValue: PropTypes.number
+  initValue: PropTypes.number,
+  onUpdate:PropTypes.func
 };
+
+ClickChild.defaultProps = {
+  initValue:0,
+  onUpdate:f=>f
+}
 
 export default ClickChild;
--- first_react_app/src/ClickParent.js
+++ first_react_app/src/ClickParent.js
@@ -4,12 +4,39 @@ import ClickChild from './ClickChild.js';
 
 class ClickParent extends Component {
   
+  constructor(props){
+
+    super(props);
+
+    this.onCounterUpdate = this.onCounterUpdate.bind(this);
+
+    this.initValues = [0,10,20];
+
+    const sum = this.initValues.reduce((a,b)=>a+b,0);
+    this.state = {
+      sum
+    }
+  }
+
+  onCounterUpdate(newValue,previousValue){
+    const valueChange = newValue - previousValue;
+    this.setState({
+      sum:this.state.sum+valueChange
+    })
+  } 
+
   render(){
+    console.log("enter ClickParent render")
     return(
       <div>
-        <ClickChild caption="First" initValue={0} />
-        <ClickChild caption="Second" initValue={10} />
-        <ClickChild caption="Third" initValue={20} />
+        <ClickChild caption="First" onUpdate={this.onCounterUpdate} initValue={this.initValues[0]} />
+        <ClickChild caption="Second" onUpdate={this.onCounterUpdate} initValue={this.initValues[1]} />
+        <ClickChild caption="Third" onUpdate={this.onCounterUpdate} initValue={this.initValues[2]} />
+        <button onClick={ () => this.forceUpdate() }>
+          Click me to repaint!
+        </button>
+        <hr />
+        <div>Total Count :{this.state.sum}</div>
       </div>
     )
   }
